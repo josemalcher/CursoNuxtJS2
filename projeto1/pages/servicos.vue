ed27c16b0a4919055c3e89be1b0b85a763b5b588
Jose Malcher Jr <contato@josemalcher.net>
2022-11-09 16:50:50 -0300

    8 - Busca de dados fetch

--- projeto1/pages/servicos.vue
+++ projeto1/pages/servicos.vue
@@ -4,12 +4,29 @@
       <div class="container mx-auto">
         <h1 class="text-lg font-bold">Serviços</h1>
         <ul>
-          <li><NuxtLink to="/servicos/desenvolvimento-de-sites">Desenvolvimento sites</NuxtLink></li>
-          <li><NuxtLink to="/servicos/Sistemas-Web">Sistemas Web</NuxtLink></li>
+          <li>
+            <NuxtLink to="/servicos/desenvolvimento-de-sites">Desenvolvimento sites</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/servicos/Sistemas-Web">Sistemas Web</NuxtLink>
+          </li>
         </ul>
         <br><br>
-        <NuxtChild />
+        <NuxtChild/>
       </div>
+      <br><br>
+      <pre>{{ $fetchState }}</pre>
+
+
+      <div v-if="$fetchState.pending">
+        carregando...
+      </div>
+      <div v-else>
+        <div v-for="service in services" :key="service.id" class="border-b border-gray-400 py-4">
+          {{ service.username }}
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -17,8 +34,22 @@
 <script>
 export default {
   name: "servicos",
-  middleware() {
-    console.log('middleware SERVICOS ATIVADO...')
+  // middleware() {
+  //   console.log('middleware SERVICOS ATIVADO...')
+  // },
+  data() {
+    return {
+      services: []
+    }
+  },
+  async fetch() {
+    // await new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve()
+    //   }, 3000)
+    // })
+    // console.log(this)
+    this.services = await this.$axios.$get('https://jsonplaceholder.typicode.com/users')
   }
 }
 </script>
